b8b6c2c3c46438d6f8e1e316d739bd9ac6f4ce83
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-09-26 09:49:57 +0000

    作成

---
+++ app/assets/javascripts/slick.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1500,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+  });
+});
+
